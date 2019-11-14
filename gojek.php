f890f923adc93322d3ec4cc8b03c395ef014c426
raufsl <57763124+raufsl@users.noreply.github.com>
2019-11-14 22:25:48 +0700

    Add files via upload

--- gojek.php
+++ gojek.php
@@ -57,7 +57,7 @@ if ($register == false)
             if ($claim == false) {
                 echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
                 sleep(3);
-                echo "\e[93m[!] Trying to redeem Voucher : GOFOODBOBA19 !\n";
+                echo "\e[93m[!] Trying to redeem Voucher : GOFOODSANTAI08 !\n";
                 sleep(3);
                 goto next1;
             }
